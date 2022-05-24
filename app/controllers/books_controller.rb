c50af26bf7feb750ee0400db0c5a7534fdbb9ae1
EC2 Default User <ec2-user@ip-172-31-15-49.ec2.internal>
2022-05-24 10:38:52 +0000

    [Add]book_showページを追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -11,11 +11,12 @@ class BooksController < ApplicationController
 
   def index
     @books = Book.all
-    @book = Book.new
     @user = current_user
   end
 
   def show
+    @book = Book.find(params[:id])
+    @user = @book.user
   end
 
   def edit
