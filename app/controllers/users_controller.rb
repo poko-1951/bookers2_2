c74d5e908922c7d12ec53203dfb68fcf547bd210
EC2 Default User <ec2-user@ip-172-31-15-49.ec2.internal>
2022-05-24 09:42:00 +0000

    [Add]部分テンプレートの追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,12 +3,13 @@ class UsersController < ApplicationController
   def index
     @user = User.find(current_user.id)
     @users = User.all
+    @book = Book.new
   end
 
   def show
     @user = User.find(params[:id])
     @book = Book.new
-    @books = Book.all
+    @books = Book.where(user_id: @user.id)
   end
 
   def edit
